e4994c740d8153abfbc6bede88c63af1fe3d70aa
JanisPetersons83 <144663195+JanisPetersons83@users.noreply.github.com>
2026-09-01 14:31:00 +0300

    Update service worker cache version to v2-0-83

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = "inventory-app-v2-0-82"; // 🔥 MAINI ŠO katru update!
+const CACHE_NAME = "inventory-app-v2-0-83"; // 🔥 MAINI ŠO katru update!
 
 const BASE = "/Inventory-app";
 const urlsToCache = [
