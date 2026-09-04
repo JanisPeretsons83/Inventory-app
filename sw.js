aa6f5bb14d18523c82c56ee57a76962ca476bd01
JanisPetersons83 <144663195+JanisPetersons83@users.noreply.github.com>
2026-09-04 22:37:35 +0300

    Update service worker to version 1-4

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // 📦 SERVICE WORKER
 // ======================================================
 
-const CACHE_NAME = "inventory-app-v2-1-3";
+const CACHE_NAME = "inventory-app-v2-1-4";
 const BASE = "/Inventory-app";
 const urlsToCache = [
 
