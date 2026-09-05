b966e0467567f5b237f890ed87d5f79812031a0f
JanisPetersons83 <144663195+JanisPetersons83@users.noreply.github.com>
2026-09-05 23:28:10 +0300

    Update service worker cache version to v2-1-16

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // 📦 SERVICE WORKER
 // ======================================================
 
-const CACHE_NAME = "inventory-app-v2-1-15";
+const CACHE_NAME = "inventory-app-v2-1-16";
 const BASE = "/Inventory-app";
 const urlsToCache = [
 
